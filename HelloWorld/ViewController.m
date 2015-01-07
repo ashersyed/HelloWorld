f7cf8066b45b7507f77c72039ae17ccebe5720fd
Asher Syed <ashersyed@macmini.local>
2015-01-07 14:29:08 +0500

    non linked fields error

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property(nonatomic, retain) IBOutlet UILabel*      lblID;
+@property(nonatomic, retain) IBOutlet UITextField*  txtID;
+@property(nonatomic, retain) IBOutlet UILabel*      lblPwd;
+@property(nonatomic, retain) IBOutlet UITextField*  txtPwd;
+@property(nonatomic, retain) IBOutlet UIButton*     btnSignIn;
+
 @end
 
 @implementation ViewController
